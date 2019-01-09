80c1d800d49720de42f8dc13891de8c07c6f71b2
Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
2019-01-09 15:06:58 +0100

    Update __init__.py to include OptimadeStructureImport

--- aiidalab_optimade/__init__.py
+++ aiidalab_optimade/__init__.py
@@ -6,6 +6,6 @@ AiiDA Lab App that implements an OPTiMaDe client
 
 # pylint: disable=unused-import
 from .importer import OptimadeImporter  # noqa
-from .optimade import OptimadeWidget  # noqa
+from .optimade import OptimadeStructureImport  # noqa
 
 __version__ = "0.1-alpha"
