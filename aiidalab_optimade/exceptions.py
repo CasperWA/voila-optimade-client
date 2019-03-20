0fee2992caff3ca0f5bb9328e8bf747fa7475f72
Casper Welzel Andersen <casper.andersen@epfl.ch>
2019-03-20 18:28:15 +0100

    Addition of `--no-use-pep517` for travis.

--- aiidalab_optimade/exceptions.py
+++ aiidalab_optimade/exceptions.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 
+
 class ApiVersionError(Exception):
     """ API Version Error
-
     The API version does not have the correct semantic.
     The API version cannot be recognized.
     """
     pass
 
+
 class InputError(Exception):
     """ Input Error
-
     Base input error exception.
     Wrong input to a method.
     """
     pass
 
+
 class DisplayInputError(InputError):
     """ Display Input Error
-
     The input to display method cannot be used.
     If 'all' is True, 'part' must be None.
     """
