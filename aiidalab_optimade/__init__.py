d4a247e77d549b5956ff540801f1db997d44512c
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-01-18 18:45:36 +0100

    Remove traces of importer.py

--- aiidalab_optimade/__init__.py
+++ aiidalab_optimade/__init__.py
@@ -6,7 +6,6 @@ AiiDA Lab App that implements an OPTiMaDe client
 import json
 from pathlib import Path
 
-from aiidalab_optimade.importer import OptimadeImporter  # noqa
 from aiidalab_optimade.optimade_query import OptimadeQueryWidget  # noqa
 
 PATH_TO_METADATA = Path(__file__).parent.parent.joinpath("metadata.json").resolve()
