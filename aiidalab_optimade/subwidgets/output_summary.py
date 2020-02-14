28fa41d949c027a1ae90e4466c32a58ece4e36ec
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-02-15 00:02:10 +0100

    Show structure sites in a results output tab

--- aiidalab_optimade/subwidgets/output_summary.py
+++ aiidalab_optimade/subwidgets/output_summary.py
@@ -1,16 +1,18 @@
 import re
-from typing import Match
+from typing import Match, List
 import ipywidgets as ipw
 import traitlets
+
+import pandas as pd
 
 from aiida.orm import StructureData
 
 
-__all__ = ("StructureDataOutput",)
+__all__ = ("StructureDataSummary", "StructureDataSites")
 
 
-class StructureDataOutput(ipw.VBox):
-    """ Structure Data Output Widget
+class StructureDataSummary(ipw.VBox):
+    """StructureData Summary Output
     Show structure data as a set of HTML widgets in a VBox widget.
     """
 
@@ -95,3 +97,77 @@ class StructureDataOutput(ipw.VBox):
         out += r" \end{smallmatrix} \Bigr)$"
 
         return out
+
+
+class StructureDataSites(ipw.HTML):
+    """StructureData Sites Output
+    Reimplements the viewer for AiiDA Dicts
+    """
+
+    structure = traitlets.Instance(StructureData, allow_none=True)
+
+    def __init__(self, structure: StructureData = None, **kwargs):
+        # For more information on how to controle the table appearance please visit:
+        # https://css-tricks.com/complete-guide-table-element/
+        self._style = """
+        <style>
+            .df { border: none; }
+            .df tbody tr:nth-child(odd) { background-color: #e5e7e9; }
+            .df tbody tr:nth-child(odd):hover { background-color:   #f5b7b1; }
+            .df tbody tr:nth-child(even):hover { background-color:  #f5b7b1; }
+            .df tbody td { min-width: 300px; text-align: center; border: none }
+            .df th { text-align: center; border: none;  border-bottom: 1px solid black;}
+        </style>
+        """
+        pd.set_option("max_colwidth", 100)
+
+        super().__init__(layout=ipw.Layout(width="auto"), **kwargs)
+        self.observe(self._on_change_structure, names="structure")
+        self.structure = structure
+
+    def _on_change_structure(self, change: dict):
+        """When traitlet 'sites' is updated"""
+        if change["new"] is None:
+            self.value = ""
+        else:
+            self.value = self._style
+            dataf = pd.DataFrame(
+                self._format_sites(), columns=["Elements", "Occypancy", "Position"]
+            )
+            # self.value += dataf.to_string()
+            self.value += dataf.to_html(classes="df", index=False)
+
+    def freeze(self):
+        """Disable widget"""
+
+    def unfreeze(self):
+        """Activate widget (in its current state)"""
+
+    def reset(self):
+        """Reset widget"""
+        self.structure = None
+
+    def _format_sites(self) -> List[str]:
+        """Format AiiDA StructureData into list of formatted HTML strings"""
+        res = []
+        for site in self.structure.sites:
+            for kind in self.structure.kinds:
+                if kind.name == site.kind_name:
+                    site_kind = kind
+                    break
+            else:
+                raise Exception(
+                    f"Kind cannot be found for site: {site}. Kinds: {self.structure.kinds}"
+                )
+            occupancies = site_kind.weights
+            if re.match(r".*X[^e]", site.kind_name):
+                occupancies.append(round(1.0 - sum(occupancies), 2))
+
+            res.append(
+                (
+                    ", ".join([str(_) for _ in site_kind.symbols]),
+                    ", ".join([str(_) for _ in occupancies]),
+                    str(site.position),
+                )
+            )
+        return res
