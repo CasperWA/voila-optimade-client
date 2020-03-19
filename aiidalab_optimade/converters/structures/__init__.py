2ac26da065578e6a1e450a7a78d542195b17f764
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-03-19 16:31:30 +0100

    Add converter for OPTIMADE structure to pymatgen
    This will be either a pymatgen.Structure or pymatgen.Molecule, depending
    on the dimension_types. If it's a bulk structure (i.e.,
    dimension_types=(1,1,1), a pymatgen.Structure is created and returned,
    if not, a pymatgen.Molecule is created and returned).

--- aiidalab_optimade/converters/structures/__init__.py
+++ aiidalab_optimade/converters/structures/__init__.py
@@ -1,5 +1,7 @@
 # pylint: disable=undefined-variable
 from .aiida import *
+from .ase import *
+from .pymatgen import *
 
 
-__all__ = aiida.__all__
+__all__ = aiida.__all__ + ase.__all__ + pymatgen.__all__
