35b76169cafd323e940ff082a9833444b6c7886d
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-05-23 01:40:47 +0200

    Use periodic table widget for elements input
    Wrap PTableWidget in a custom widget.
    This makes it easier to use in FilterInputs as well as to toggle HAS ANY
    or HAS ALL filterings.
    
    Wrap `collect_value` in a try-except in order to properly raise if an
    exception occurs.
    
    Allow the filter parser to return operators.
    OPERATORS_MAP has become a fallback, with the default operator being
    `""`.

--- optimade_client/subwidgets/__init__.py
+++ optimade_client/subwidgets/__init__.py
@@ -2,6 +2,7 @@
 from .filter_inputs import *
 from .multi_checkbox import *
 from .output_summary import *
+from .periodic_table import *
 from .provider_database import *
 from .results import *
 
@@ -10,6 +11,7 @@ __all__ = (
     filter_inputs.__all__  # noqa
     + multi_checkbox.__all__  # noqa
     + output_summary.__all__  # noqa
+    + periodic_table.__all__  # noqa
     + provider_database.__all__  # noqa
     + results.__all__  # noqa
 )
