64c1f1703bbb2d68859a6455b291af7ee866848e
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-05-23 00:13:52 +0200

    Resize logo on start page

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ TEMPLATE = """
 <p>Use OPTIMADE to find a structure in common materials databases</p>
 <div align="center">
   <a href="{appbase}/OPTIMADE_general.ipynb" target="_blank">
-    <img src="{appbase}/img/optimade-text-right-transparent-bg.png" alt="OPTIMADE: Open Databases Integration for Materials Design">
+    <img src="{appbase}/img/optimade-text-right-transparent-bg.png" alt="OPTIMADE: Open Databases Integration for Materials Design" width="375px" height="137.5px">
   </a>
 </div>
 """
