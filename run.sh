cc2246d0b868c2bfe8c0ff915f003399cad7101f
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-05-22 16:46:51 +0200

    Add materialscloud Voilà template
    Extend run.sh with updating the symlinked materialscloud-voila-template
    repository (now a git submodule) and copy the template into the correct
    jupyter data dir with /voila/templates/

--- run.sh
+++ run.sh
@@ -4,5 +4,12 @@
 jupyter_config_dir=$(jupyter --config-dir)
 cp -f ./jupyter_config.json "${jupyter_config_dir}/voila.json"
 
+# Update sub-module
+git submodule init
+git submodule update
+
+# Copy template to Jupyter data dir
+./copy_voila_template.py materialscloud
+
 # Start the tornado voila server (no browser)
 voila --no-browser OPTIMADE_general.ipynb
