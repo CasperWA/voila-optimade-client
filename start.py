9584e7f7d99f11c8a7cfa92b0b1e76da7d7f55dc
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-05-27 17:27:49 +0200

    Move text to be hover text instead

--- start.py
+++ start.py
@@ -1,7 +1,6 @@
 TEMPLATE = """
-<p>Use OPTIMADE to find a structure in common materials databases</p>
 <div align="center">
-  <a href="{appbase}/OPTIMADE_general.ipynb" target="_blank">
+  <a href="{appbase}/OPTIMADE_general.ipynb" target="_blank" title="Use OPTIMADE to find a structure in common materials databases">
     <img src="{appbase}/img/optimade-text-right-transparent-bg.png" alt="OPTIMADE: Open Databases Integration for Materials Design" width="375px" height="137.5px">
   </a>
 </div>
