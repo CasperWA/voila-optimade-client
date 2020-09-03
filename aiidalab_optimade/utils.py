19517742fa587ffef2c12f3f5fd999b4ec548987
Simon Adorf <simon.adorf@epfl.ch>
2020-09-03 15:15:02 +0000

    Only create cache dir once per interpreter session.

--- aiidalab_optimade/utils.py
+++ aiidalab_optimade/utils.py
@@ -35,11 +35,12 @@ PROVIDERS_URLS = [
     "/links/v1/providers.json",
 ]
 
-AUTHOR = "AiiDAlab Team"
+APP_NAME = "OPTIMADE"
+APP_AUTHOR = "AiiDAlab Team"
 
-CACHED_PROVIDERS = Path(appdirs.user_cache_dir("OPTIMADE", AUTHOR)).joinpath(
-    "cached_providers.json"
-)
+CACHE_DIR = Path(appdirs.user_cache_dir(APP_NAME, APP_AUTHOR))
+CACHE_DIR.mkdir(parents=True, exist_ok=True)
+CACHED_PROVIDERS = CACHE_DIR / "cached_providers.json"
 
 
 def perform_optimade_query(  # pylint: disable=too-many-arguments,too-many-branches,too-many-locals
@@ -165,7 +166,6 @@ def update_local_providers_json(response: dict) -> None:
     LOGGER.debug(
         "Creating/updating local file of cached providers (%r).", CACHED_PROVIDERS.name
     )
-    CACHED_PROVIDERS.parent.mkdir(parents=True, exist_ok=True)
     with open(CACHED_PROVIDERS, "w") as handle:
         json.dump(_response, handle)
 
