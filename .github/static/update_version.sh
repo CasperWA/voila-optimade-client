e51fe6d530bdc05a0047c0a2dacea69430411bf7
Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
2020-09-15 16:46:02 +0200

    Publish through GitHub release (#150)
    Use CasperWA/push-protected to ensure the version is updated in the code
    base accordingly.

---
+++ .github/static/update_version.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+echo "\n### Checkout fresh branch ###"
+git checkout -b update_version
+
+echo "\n### Setting commit user ###"
+git config --local user.email "casper.andersen@epfl.ch"
+git config --local user.name "CasperWA"
+
+echo "\n### Install invoke ###"
+pip install -U invoke
+
+echo "\n### Update version ###"
+invoke update-version --version="${GITHUB_REF#refs/tags/}"
+
+echo "\n### Commit updated files ###"
+git add setup.py
+git add optimade_client/version.py
+git commit -m "Release ${GITHUB_REF#refs/tags/}"
