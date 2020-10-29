3a425d12bc735435e5cea4969bdcb6dd53ab98a5
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-10-29 10:26:29 +0100

    Update publish workflow
    Do all changes in `update_version.sh`, including updating the tag.
    
    Use the new `force` and `tags` flags from `CasperWA/push-protected@v2`
    action.
    
    Temporarily comment out the steps actually publishing to PyPI to first
    run some tests of the updated workflow.

--- .github/static/update_version.sh
+++ .github/static/update_version.sh
@@ -1,22 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 
-echo "\n### Checkout fresh branch ###"
-git checkout -b update_version
-
-echo "\n### Setting commit user ###"
+echo -e "\n### Setting commit user ###"
 git config --local user.email "casper.andersen@epfl.ch"
 git config --local user.name "CasperWA"
 
-echo "\n### Install invoke ###"
+echo -e "\n### Install invoke ###"
 pip install -U invoke
 
-echo "\n### Update version ###"
+echo -e "\n### Update version ###"
 invoke update-version --version="${GITHUB_REF#refs/tags/}"
 
-echo "\n### Commit updated files ###"
-git add setup.py
-git add optimade_client/__init__.py
-git add optimade_client/informational.py
-git add optimade_client/cli/run.py
-git commit -m "Release ${GITHUB_REF#refs/tags/}"
+echo -e "\n### Commit updated files ###"
+git commit -am "Release ${GITHUB_REF#refs/tags/}"
+
+echo -e "\n### Update new version tag ###"
+TAG_MSG=.github/static/release_tag_msg.txt
+sed -i "s|TAG_NAME|${GITHUB_REF#refs/tags/}|g" "${TAG_MSG}"
+git tag -af -F "${TAG_MSG}" ${GITHUB_REF#refs/tags/}
