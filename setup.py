e0426bc403e68bd97803b9344a6f07ac05906a00
Casper Welzel Andersen <casper.andersen@epfl.ch>
2021-03-26 10:30:37 +0100

    Add Python 3.9 support

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Database :: Front-Ends",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Chemistry",
