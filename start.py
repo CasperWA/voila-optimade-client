a61ebd3aea2c0dd5b9f2a0b0c5efba80eb0f1763
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2021-08-06 09:50:27 +0200

    Loosen requirements for AiiDAlab
    Allow earlier versions of nglview and requests.
    
    Update and unify notebook taken from aiidalab-optimade repo.

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 TEMPLATE = """
 <div align="center">
-  <a href="{appbase}/OPTIMADE Client.ipynb" target="_blank" title="OPTIMADE Client">
+  <a href="{appbase}/OPTIMADE-Client.ipynb" target="_blank" title="OPTIMADE Client">
     <img src="{appbase}/img/optimade-text-right-transparent-bg.png" alt="OPTIMADE: Open Databases Integration for Materials Design" width="375px" height="137.5px">
   </a>
 </div>
