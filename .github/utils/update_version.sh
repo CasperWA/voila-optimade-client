59c253a173b9c4604a7fef726d5b157a01ea9b5f
Casper Welzel Andersen <casper.andersen@epfl.ch>
2021-10-21 00:06:54 +0200

    Update filenames in .github folder

--- .github/utils/update_version.sh
+++ .github/utils/update_version.sh
@@ -15,6 +15,6 @@ echo -e "\n### Commit updated files ###"
 git commit -am "Release ${GITHUB_REF#refs/tags/}"
 
 echo -e "\n### Update new version tag ###"
-TAG_MSG=.github/static/release_tag_msg.txt
+TAG_MSG=.github/utils/release_tag_msg.txt
 sed -i "s|TAG_NAME|${GITHUB_REF#refs/tags/}|g" "${TAG_MSG}"
 git tag -af -F "${TAG_MSG}" ${GITHUB_REF#refs/tags/}
