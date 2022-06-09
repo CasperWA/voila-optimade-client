27fc57a9495d8dd7fd4bd1721933e3eeecf68ce5
CasperWA <casper+github@welzel.nu>
2022-06-09 14:44:17 +0000

    Release 2022.6.9

--- optimade_client/cli/run.py
+++ optimade_client/cli/run.py
@@ -12,7 +12,7 @@ except ImportError:
 
 
 LOGGING_LEVELS = [logging.getLevelName(level).lower() for level in range(0, 51, 10)]
-VERSION = "2022.6.8"  # Avoid importing optimade-client package
+VERSION = "2022.6.9"  # Avoid importing optimade-client package
 
 
 def main(args: list = None):
